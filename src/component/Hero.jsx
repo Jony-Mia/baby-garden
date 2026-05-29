7745424373dd46813a0e801bd63d502ee7e138a4
Jony-Mia <ajonymia321@gmail.com>
2026-05-29 22:16:28 +0600

    hero section added

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import { bubblegum_sans } from '@/app/layout';
+// import Cloud from "@/assets/cloud.png"
+import { Button } from '@heroui/react';
+import HeroBaby from "@/assets/drawing_2.png"
+import SunMoon from "@/assets/sun_moon.png"
+import Image from 'next/image';
 
 const Hero = () => {
     return (
         <div>
-            <div>
+            <div className='grid grid-cols-2 p-5 md:grid-cols-2 sm:grid-cols-1 '>
+                <div className={`flex flex-col justify-center items-center`}>
+                    <div>
+                        <Image height={"100"} src={SunMoon} alt='Sun and Moon'  />
+                    </div> <br />
+                    <h1 className={`${bubblegum_sans.className} text-[#5693CA] capitalize text-6xl`}>be kind <br /> with kids</h1> <br />
+                    <p>Welcome to the Baby Garden AG child day care. <br /> Exclusive for kids age between 3 to 12</p> <br />
+                    <Button className={"rounded-xl px-6 py-3 h-auto w-auto bg-[#FBD683] text-black"}>Request a daycare place now</Button>
+                </div>
                 <div>
-                    <h1>be kind <br /> with kids</h1>
+                    <Image src={HeroBaby} height={"700"} alt='Hero image' />
                 </div>
             </div>
         </div>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 import { usePathname } from "next/navigation";
 import { Button, Dropdown, Label } from "@heroui/react";
-import { useState } from "react";
+
 const Navbars = () => {
     let path = usePathname();
 
@@ -16,38 +16,38 @@ const Navbars = () => {
                     <div className="">
                         <div className="flex flex-col items-center">
                             <Image src={"/baby_logo.png"} width={"45"} height={"45"} alt="logo" />
-                            <p className="font-bold text-[10px]">Baby Garden</p>
+                            <p className="font-bold sm:text-sm">Baby Garden</p>
                         </div>
                     </div>
-                    <div className="shadow-[#C6E0EA] flex items-center justify-center md:max-w-[70%] max-w-[50%] w-full rounded-full shadow flex-wrap h-[60px] px-5">
+                    <div className="shadow-[#C6E0EA] flex items-center justify-center md:max-w-[70%] max-w-[50%] w-[110% rounded-full shadow flex-wrap h-[60px] px-5">
                         {/* <div > */}
-                        <ul className="flex flex-wrap items-center gap-5">
+                        <ul className="flex flex-wrap items-center gap-1">
                             <li>
-                                <Link href="/" className={`hover:bg-[#FBD683] rounded-2xl ${path === '/' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                <Link href="/" className={`hover:bg-[#FBD683] rounded-2xl ${path === '/' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all sm:text-sm`}>
                                     <i className="fad fa-home mr-1"></i>
                                     Home
                                 </Link>
                             </li>
                             <li>
-                                <Link href="/about" className={`hover:bg-[#FBD683] rounded-2xl ${path === '/about' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                <Link href="/about" className={`hover:bg-[#FBD683] rounded-2xl ${path === '/about' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all sm:text-sm`}>
                                     <i className="fad fa-circle-info mr-1"></i>
                                     About
                                 </Link>
                             </li>
                             <li>
-                                <Link href={"/contact"} className={`hover:bg-[#FBD683] rounded-2xl ${path === '/contact' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                <Link href={"/contact"} className={`hover:bg-[#FBD683] rounded-2xl ${path === '/contact' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all sm:text-sm`}>
                                     <i className="fa-duotone fa-thin fa-address-book mr-1"></i>
                                     Contact
                                 </Link>
                             </li>
                             <li>
-                                <Link href={"/babies"} className={`hover:bg-[#fbd583bc] rounded-2xl ${path === '/babies' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                <Link href={"/babies"} className={`hover:bg-[#fbd583bc] rounded-2xl ${path === '/babies' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all sm:text-sm`}>
                                     <i className="fad fa-baby mr-1"></i>
                                     Babies
                                 </Link>
                             </li>
                             <li>
-                                <Link href={"/addBaby"} className={`hover:bg-[#FBD683] rounded-2xl ${path === '/registration' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                <Link href={"/addBaby"} className={`hover:bg-[#FBD683] rounded-2xl ${path === '/registration' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all sm:text-sm`}>
                                     <i className="fad fa-baby mr-1"></i>
                                     Registration
                                 </Link>
@@ -55,7 +55,8 @@ const Navbars = () => {
                         </ul>
                         {/* </div> */}
                     </div>
-                    <div>
+                    <div className="md:block hidden">
+
                         <ul className="flex w-full items-center gap-2 border-[#FBD683] border p-2 rounded-3xl">
                             <li><Link href="/login" className={` rounded-2xl ${path === '/login' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
                                 <Button className={"bg-[#FBD683] text-black "}>
@@ -72,6 +73,27 @@ const Navbars = () => {
                             </li>
                         </ul>
                     </div>
+                    <div className="md:hidden block ">
+
+                        <Dropdown className=" w-full border-[#FBD683] border p-2 rounded-3xl">
+                            <Button aria-label="Menu" variant="outline">
+                                <i className="fa-duotone fa-solid fa-bars"></i>
+                            </Button>
+                            <Dropdown.Popover>
+                                <Dropdown.Menu onAction={(key) => console.log(`Selected: ${key}`)}>
+                                    {/* <ul className="flex flex-wrap items-center gap-5"> */}
+                                   
+                                    <Dropdown.Item href={"/addBaby"} className={`flex w-full rounded-2xl ${path === '/registration' ? "border-2 border-[#C6E0EA]" : ""} px-3 text-sm py-2 transition-all`} >
+                                        {/* <Link > */}
+                                            <i className="fad fa-baby mr-1"></i>
+                                            Registration
+                                        {/* </Link> */}
+                                    </Dropdown.Item>
+                                    {/* </ul> */}
+                                </Dropdown.Menu>
+                            </Dropdown.Popover>
+                        </Dropdown>
+                    </div>
                 </header>
             </nav>
             <MobileMenu />
@@ -84,7 +106,6 @@ export default Navbars;
 
 export function MobileMenu() {
     let path = usePathname();
-
 
     return (
         <nav className="md:hidden sticky top-0 z-40 w-full border-b border-separator bg-background/70 backdrop-blur-lg">
@@ -100,36 +121,36 @@ export function MobileMenu() {
                         <Dropdown.Popover>
                             <Dropdown.Menu onAction={(key) => console.log(`Selected: ${key}`)}>
                                 {/* <ul className="flex flex-wrap items-center gap-5"> */}
-                                    <Dropdown.Item>
-                                        <Link href="/" className={` w-full rounded-2xl ${path === '/' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
-                                            <i className="fad fa-home mr-1"></i>
-                                            Home
-                                        </Link>
-                                    </Dropdown.Item>
-                                    <Dropdown.Item>
-                                        <Link href="/about" className={` w-full rounded-2xl ${path === '/about' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
-                                            <i className="fad fa-circle-info mr-1"></i>
-                                            About
-                                        </Link>
-                                    </Dropdown.Item>
-                                    <Dropdown.Item>
-                                        <Link href={"/contact"} className={` w-full rounded-2xl ${path === '/contact' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
-                                            <i className="fa-duotone fa-thin fa-address-book mr-1"></i>
-                                            Contact
-                                        </Link>
-                                    </Dropdown.Item>
-                                    <Dropdown.Item>
-                                        <Link href={"/babies"} className={`hover:bg-[#fbd583bc]  w-full rounded-2xl ${path === '/babies' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
-                                            <i className="fad fa-baby mr-1"></i>
-                                            Babies
-                                        </Link>
-                                    </Dropdown.Item>
-                                    <Dropdown.Item>
-                                        <Link href={"/addBaby"} className={` w-full rounded-2xl ${path === '/registration' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
-                                            <i className="fad fa-baby mr-1"></i>
-                                            Registration
-                                        </Link>
-                                    </Dropdown.Item>
+                                <Dropdown.Item href="/" className={` w-full rounded-2xl ${path === '/' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                    {/* <Link > */}
+                                        <i className="fad fa-home mr-1"></i>
+                                        Home
+                                    {/* </Link> */}
+                                </Dropdown.Item>
+                                <Dropdown.Item href="/about" className={` w-full rounded-2xl ${path === '/about' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                    {/* <Link > */}
+                                        <i className="fad fa-circle-info mr-1"></i>
+                                        About
+                                    {/* </Link> */}
+                                </Dropdown.Item>
+                                <Dropdown.Item  href={"/contact"} className={` w-full rounded-2xl ${path === '/contact' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                    {/* <Link> */}
+                                        <i className="fa-duotone fa-thin fa-address-book mr-1"></i>
+                                        Contact
+                                    {/* </Link> */}
+                                </Dropdown.Item>
+                                <Dropdown.Item  href={"/babies"} className={`hover:bg-[#fbd583bc]  w-full rounded-2xl ${path === '/babies' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                    {/* <Link> */}
+                                        <i className="fad fa-baby mr-1"></i>
+                                        Babies
+                                    {/* </Link> */}
+                                </Dropdown.Item>
+                                <Dropdown.Item href={"/addBaby"} className={` w-full rounded-2xl ${path === '/registration' ? "border-2 border-[#C6E0EA]" : ""} px-3 py-2 transition-all`}>
+                                    {/* <Link > */}
+                                        <i className="fad fa-baby mr-1"></i>
+                                        Registration
+                                    {/* </Link> */}
+                                </Dropdown.Item>
                                 {/* </ul> */}
                             </Dropdown.Menu>
                         </Dropdown.Popover>
